f5dad7f31e765b1dafab03a4f0785cc4d0fde8db
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-10-24 11:27:14 +0200

    Fix win condition bug

--- src/main/java/model/rules/DrawFavorsDealerStrategy.java
+++ src/main/java/model/rules/DrawFavorsDealerStrategy.java
@@ -11,10 +11,9 @@ public class DrawFavorsDealerStrategy implements WinningStrategy {
    * Checks scores for Player and Dealer, Dealer wins if draw.
    */
   public boolean dealerWon(Player player, Player dealer, int maxScore) {
-    
     if (player.calcScore() > maxScore) {
       return true;
-    } else if (player.calcScore() > maxScore) {
+    } else if (dealer.calcScore() > maxScore) {
       return false;
     }
     return dealer.calcScore() >= player.calcScore();
